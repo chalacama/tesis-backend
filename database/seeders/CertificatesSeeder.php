3931c606b8e3a9befe056c5bb710699ea348523f
Chala <filyp007lfcm117@gmail.com>
2025-11-18 19:59:32 -0500

    seeder de certificados

--- database/seeders/CertificatesSeeder.php
+++ database/seeders/CertificatesSeeder.php
@@ -21,12 +21,42 @@ class CertificatesSeeder extends Seeder
             'total_score'     => 95, // puedes ajustar la puntuación que quieras
         ]);
 
+        Certificate::create([
+            'registration_id' => 1,
+            'code'            => 'CERT-' . strtoupper(Str::random(8)),
+            'total_score'     => 100, // otra puntuación de ejemplo
+        ]);
+
         // Creamos el certificado para el user 2 (curso 1)
         Certificate::create([
             'registration_id' => 4,
             'code'            => 'CERT-' . strtoupper(Str::random(8)),
             'total_score'     => 75, // otra puntuación de ejemplo
         ]);
+        Certificate::create([
+            'registration_id' => 4,
+            'code'            => 'CERT-' . strtoupper(Str::random(8)),
+            'total_score'     => 70, // otra puntuación de ejemplo
+        ]);
+
+        Certificate::create([
+            'registration_id' => 4,
+            'code'            => 'CERT-' . strtoupper(Str::random(8)),
+            'total_score'     => 50, // otra puntuación de ejemplo
+        ]);
+
+        Certificate::create([
+            'registration_id' => 3,
+            'code'            => 'CERT-' . strtoupper(Str::random(8)),
+            'total_score'     => 60, // otra puntuación de ejemplo
+        ]);
+
+        Certificate::create([
+            'registration_id' => 3,
+            'code'            => 'CERT-' . strtoupper(Str::random(8)),
+            'total_score'     => 60, // otra puntuación de ejemplo
+        ]);
+
         
     }
 }
