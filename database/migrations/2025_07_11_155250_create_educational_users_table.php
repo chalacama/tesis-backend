ef2ad26756f4b4fa850529dbed5c147077f40959
Chala <filyp007lfcm117@gmail.com>
2026-05-22 21:18:38 -0500

    hola mundo 6543

--- database/migrations/2025_07_11_155250_create_educational_users_table.php
+++ database/migrations/2025_07_11_155250_create_educational_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->unsignedBigInteger('career_id')->nullable();
             $table->unsignedBigInteger('educational_level_id')->nullable();
             $table->integer('level')->nullable();
+            $table->boolean('finished')->default(false);
+        
             $table->timestamps();
 
             $table->foreign('sede_id')->references('id')->on('sedes')->onDelete('cascade');
